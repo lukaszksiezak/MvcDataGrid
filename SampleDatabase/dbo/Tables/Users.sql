f5a0c6ed6ca09741ebf432ef58873c26102079e9
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-01-24 19:04:28 +0100

    Creation of sample CRMData Database to feed datagrid

---
+++ SampleDatabase/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UsersID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]           VARCHAR (255) NULL,
+    [PhoneNumber]    VARCHAR (100) NULL,
+    [Email]          VARCHAR (255) NULL,
+    [BirthDate]      VARCHAR (255) NULL,
+    [AddressStreet]  VARCHAR (255) NULL,
+    [AddressCity]    VARCHAR (255) NULL,
+    [AddressCountry] VARCHAR (100) NULL,
+    [Company]        VARCHAR (255) NULL,
+    [StartedWorking] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([UsersID] ASC)
+);
+
