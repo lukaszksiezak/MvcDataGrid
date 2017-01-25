87c21c7cd2205819eb6bf84d9bf629dcc0f7d86c
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-01-25 22:51:59 +0100

    Edit form added

--- MvcGrid/Controllers/HomeController.cs
+++ MvcGrid/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcDatagridSample.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,11 @@ namespace MvcDatagridSample.Controllers
 
             var users = context.Users.ToList();
             return View(users);
+        }
+
+        public ActionResult EditUser(Users user)
+        {
+            return View(user);
         }
     }
 }
