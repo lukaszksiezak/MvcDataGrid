c2fdfcae7fd448b8cd28edc06b3531d0153b998f
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-02-01 21:56:24 +0100

    Small cleanup to the code

--- MvcGrid/Controllers/HomeController.cs
+++ MvcGrid/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using MvcDatagridSample.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MvcDatagridSample.Controllers
